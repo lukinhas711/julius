e5eb7fa0c6dc9aa2110bc09cb8526ddb3ffa292b
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-04-23 16:58:54 -0300

    chore: app head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["@/assets/css/main.css"],
+  app: {
+    head: {
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+      charset: 'utf-8',
+    },
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
