ef92892b61626c0aa7ff9ba6a3d9feb8b8828dba
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2023-04-24 09:33:22 -0300

    chore: remove text spacing

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <template #icon> <ExclamationTriangleIcon class="h-6 w-6" /> </template>
       <template #text> Você precisa estar logado para interagir </template>
       <template #button>
-        <BaseButton to="/login"> Fazer Login! </BaseButton>
+        <BaseButton to="/login">Fazer Login!</BaseButton>
       </template>
     </BaseAlert>
   </div>
